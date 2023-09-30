e5242303897f28481d9d7cc76d6b6ec56807d308
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-09-30 16:42:25 +0530

    Add

--- src/Script.js
+++ src/Script.js
@@ -10,10 +10,15 @@ var jumpAnimationNumber=0;
 var jumpImageNumber=1;
 var robotMarginTop=472;
 var boxMarginLeft=1400;
+var boxMarginLeft1=1400;
 var boxAnimationId=0;
+var boxAnimationId1=0;
 var box;
+var box2;
 var currentMarginLeft;
+var currentMarginLeft1;
 var newMarginLeft;
+var newMarginLeft1;
 
 
 
@@ -69,6 +74,10 @@ function keyCheck(event){
         if(boxAnimationId===0){
             boxAnimationId=setInterval(boxAnimation,100);
         }
+
+        if(boxAnimationId1===0){
+            boxAnimationId1=setInterval(boxAnimation2,100);
+        }
     }
 
     if(keyCode ===32){
@@ -83,6 +92,9 @@ function keyCheck(event){
 
     if(boxAnimationId===0){
         boxAnimationId=setInterval(boxAnimation,100);
+    }
+    if(boxAnimationId1===0){
+        boxAnimationId1=setInterval(boxAnimation2,100);
     }
 
 }
@@ -221,3 +233,87 @@ function boxAnimation() {
     }
 }
 
+
+function createBoxes2(){
+
+    for (var i=0; i<=150; i++){
+
+
+        var box = document.createElement("div2");
+        box.className="box2";
+        document.getElementById("background").appendChild(box);
+        box.style.marginLeft=boxMarginLeft1+"px";
+        box.id="box2"+i;
+
+        if(i<5){
+            boxMarginLeft1=boxMarginLeft1+1500;
+        }
+
+        if(i>=5 && i<=10){
+            boxMarginLeft1=boxMarginLeft1+500;
+        }
+
+        if(i>=10 && i<=15){
+            boxMarginLeft1=boxMarginLeft1+500;
+        }
+
+        if(i>=15 && i<=25){
+            boxMarginLeft1=boxMarginLeft1+300;
+        }
+
+        if(i>=25 && i<=30){
+            boxMarginLeft1=boxMarginLeft1+200;
+        }
+
+        if(i>=30 && i<=40){
+            boxMarginLeft1=boxMarginLeft1+1500;
+        }
+        if(i>=40 && i<=50){
+            boxMarginLeft1=boxMarginLeft1+500;
+        }
+        if(i>=50 && i<=55){
+            boxMarginLeft1=boxMarginLeft1+500;
+        }
+        if(i>=55 && i<=70){
+            boxMarginLeft1=boxMarginLeft1+700;
+        }
+        if(i>=70 && i<=75){
+            boxMarginLeft1=boxMarginLeft1+300;
+        }
+
+        if(i>=75 && i<=90){
+            boxMarginLeft1=boxMarginLeft1+600;
+        }
+        if(i>=90 && i<=100){
+            boxMarginLeft1=boxMarginLeft1+1500;
+        }
+        if(i>=100 && i<=110){
+            boxMarginLeft1=boxMarginLeft1+400;
+        }
+        if(i>=110 && i<=115){
+            boxMarginLeft1=boxMarginLeft1+600;
+        }
+        if(i>=115 && i<=125){
+            boxMarginLeft1=boxMarginLeft1+300;
+        }
+        if(i>=125 && i<=130){
+            boxMarginLeft1=boxMarginLeft1+1500;
+        }
+        if(i>=130 && i<=140){
+            boxMarginLeft1=boxMarginLeft1+400;
+        }
+        if(i>=140 && i<=150){
+            boxMarginLeft1=boxMarginLeft1+1600;
+        }
+
+    }
+}
+
+function boxAnimation2() {
+    for (var i=0;i<150; i++){
+        box2 =document.getElementById("box2"+i);
+        currentMarginLeft1=getComputedStyle(box2).marginLeft;
+        newMarginLeft1=parseInt(currentMarginLeft1)-25;
+        box2.style.marginLeft=newMarginLeft1+"px";
+    }
+}
